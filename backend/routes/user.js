cabeb86c4286f3a9dc7d1074d5cf056cccd43cc9
shuklapiyush470 <76607911+shuklapiyush470@users.noreply.github.com>
2021-09-06 18:50:45 +0530

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,26 @@
 const express = require("express");
+
 const router = express.Router();
 
-const { makePayment } = require("../controllers/stripepayment");
+const {
+    getUserById,
+    getUser,
+    updateUser,
+    userPurchaseList
+} = require("../controllers/user");
+const { isAuthenticated, isSignedIn } = require("../controllers/auth");
 
-router.post("/stripe/checkout", makePayment);
+router.param("userId", getUserById);
+
+router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
+router.put("/updateUser/:userId", isSignedIn, isAuthenticated, updateUser);
+
+//User Order
+router.get(
+    "/user/orders/:userId",
+    isSignedIn,
+    isAuthenticated,
+    userPurchaseList
+);
 
 module.exports = router;
