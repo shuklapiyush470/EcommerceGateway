c1427968cf812eb19200198bddc63ee40aadb915
shuklapiyush470 <76607911+shuklapiyush470@users.noreply.github.com>
2021-09-06 18:41:34 +0530

    Create product.js

---
+++ backend/models/product.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+const { Schema } = mongoose;
+
+const productSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            maxlength: 32,
+            trim: true
+        },
+        description: {
+            type: String,
+            required: true,
+            maxlength: 2000,
+            trim: true
+        },
+        price: {
+            type: Number,
+            required: true,
+            maxlength: 32,
+            trim: true
+        },
+        variant: {
+            type: String,
+            maxlength: 32,
+            trim: true
+        },
+        category: {
+            type: ObjectId,
+            ref: "Category",
+            required: true
+        },
+        stock: {
+            type: Number
+        },
+        sold: {
+            type: Number,
+            default: 0
+        },
+        photo: {
+            data: Buffer,
+            contentType: String
+        }
+    },
+    { timestamps: true, collection: "Product" }
+);
+
+module.exports = mongoose.model("Product", productSchema);
